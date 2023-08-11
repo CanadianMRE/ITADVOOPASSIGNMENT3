919edcc09e9fa304bbe2f4cb8914b76b7c7d8416
gursh <gursh@GG-PC>
2023-08-11 14:29:00 -0600

    Possibly done havent tested

---
+++ Assignment3/src/driver/WordInfo.java
@@ -0,0 +1,53 @@
+package driver;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordInfo implements Comparable<WordInfo> {
+    private String word;
+    private List<String> files;
+    private List<Integer> lines;
+    private int occurrences;
+
+    public WordInfo(String word, String file, int lineNumber) {
+        this.word = word;
+        files = new ArrayList<>();
+        files.add(file);
+        lines = new ArrayList<>();
+        lines.add(lineNumber);
+        occurrences = 1;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public List<String> getFiles() {
+        return files;
+    }
+
+    public List<Integer> getLines() {
+        return lines;
+    }
+
+    public int getOccurrences() {
+        return occurrences;
+    }
+    public void incrementFrequency() {
+    	occurrences++;
+    }
+
+    public void addLine(String file, int lineNumber) {
+        files.add(file);
+        lines.add(lineNumber);
+    }
+
+    @Override
+    public int compareTo(WordInfo other) {
+        return word.compareTo(other.word);
+    }
+
+    @Override
+    public String toString() {
+        return word + ": " + occurrences;
+    }
+}
--- Assignment3/src/driver/WordTracker.java
+++ Assignment3/src/driver/WordTracker.java
@@ -1,10 +1,105 @@
 package driver;
+import utilities.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import exceptions.TreeException;
 
 public class WordTracker {
+    public static void main(String[] args) {
+        if (args.length < 2 || args.length > 4) {
+            printUsage();
+            return;
+        }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        String inputFile = args[0];
+        String option = args[1];
+        
+        if (!option.matches("-pf|-pl|-po")) {
+            System.out.println("Invalid option: " + option);
+            printUsage();
+            return;
+        }
 
-	}
+        BSTree<WordInfo> wordTree = new BSTree<>();
+        BufferedReader reader = null;
+        try {
+        	reader = new BufferedReader(new FileReader(inputFile));
+            String line;
+            int lineNumber = 1;
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.split("\\s+");
+                for (String word : words) {
+                    word = word.toLowerCase(); // Normalize to lowercase
+                    if (!word.isEmpty()) {
+                        WordInfo wordInfo = new WordInfo(word, inputFile, lineNumber);
+                        if (wordTree.contains(wordInfo)) {
+                            wordTree.search(wordInfo).getElement().addLine(inputFile,lineNumber);
+                        } else {
+                            wordTree.add(wordInfo);
+                        }
+                    }
+                }
+                lineNumber++;
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading the file: " + e.getMessage());
+            return;
+        } catch (TreeException e) {
+            System.out.println("Error with binary search tree: " + e.getMessage());
+            return;
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Error closing the file: " + e.getMessage());
+            }
+        }
 
+        if (option.equals("-pf")) {
+            printWordsWithFiles(wordTree);
+        } else if (option.equals("-pl")) {
+            printWordsWithFilesAndLines(wordTree);
+        } else if (option.equals("-po")) {
+            printWordsWithOccurrences(wordTree);
+        } else {
+            printUsage();
+        }
+    }
+
+    private static void printUsage() {
+        System.out.println("Usage: java -jar WordTracker.jar <input.txt> -pf/-pl/-po [-f <output.txt>]");
+    }
+
+    private static void printWordsWithFiles(BSTree<WordInfo> wordTree) {
+        Iterator<WordInfo> iterator = wordTree.inorderIterator();
+        while (iterator.hasNext()) {
+            WordInfo wordInfo = iterator.next();
+            System.out.println("Word: " + wordInfo.getWord() +
+                    ", Files: " + wordInfo.getFiles());
+        }
+    }
+
+    private static void printWordsWithFilesAndLines(BSTree<WordInfo> wordTree) {
+        Iterator<WordInfo> iterator = wordTree.inorderIterator();
+        while (iterator.hasNext()) {
+            WordInfo wordInfo = iterator.next();
+            System.out.println("Word: " + wordInfo.getWord() +
+                    ", Files: " + wordInfo.getFiles() +
+                    ", Lines: " + wordInfo.getLines());
+        }
+    }
+
+    private static void printWordsWithOccurrences(BSTree<WordInfo> wordTree) {
+        Iterator<WordInfo> iterator = wordTree.inorderIterator();
+        while (iterator.hasNext()) {
+            WordInfo wordInfo = iterator.next();
+            System.out.println("Word: " + wordInfo.getWord() +
+                    ", Files: " + wordInfo.getFiles() +
+                    ", Lines: " + wordInfo.getLines() +
+                    ", Occurrences: " + wordInfo.getOccurrences());
+        }
+    }
 }
